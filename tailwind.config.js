a28b47366ca17596be3439be30c9b2a83b4598d5
Daniel Monsalve <danielmonsalve214164@correo.itm.edu.co>
2024-07-09 20:06:24 -0500

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
-/** @type {import('tailwincss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      roboto: ["Roboto", "Arial", "sans-serif"],
-      fira: ["Fira Code", "Helvetica", "monospace"],
-    },
-    colors: {
-      "legal-green": "#075148",
-      white: "#fff",
-      "legal-blue": "067dff",
-      black: "#1a1a1a",
-      "legal-red": "#8c0000",
-      "legal-cyan": "#06adc9",
-      transparent: "rgba(0, 0, 0, 0)",
-    },
-    backgroundImage: {
-      "nav-bg": "url(./public/verdeNav.avif)",
-      "serv-bg": "url(./public/pattern.avif)",
-      "contact-bg": "url(./public/patternblack.avif)",
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "Arial", "sans-serif"],
+        fira: ["Fira Code", "Helvetica", "monospace"],
+      },
+      colors: {
+        "legal-green": "#075148",
+        white: "#fff",
+        "legal-blue": "#067dff",
+        black: "#1a1a1a",
+        "legal-red": "#8c0000",
+        "legal-cyan": "#06adc9",
+        transparent: "rgba(0, 0, 0, 0)",
+      },
+      backgroundImage: {
+        "nav-bg": "url(./verdeNav.avif)",
+        "serv-bg": "url(./pattern.avif)",
+        "contact-bg": "url(./patternblack.avif)",
+      },
     },
   },
   plugins: [],
