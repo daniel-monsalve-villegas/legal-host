103a4a95f03738b6fc45b8fb176d36f67ad4733c
Daniel Monsalve <danielmonsalve214164@correo.itm.edu.co>
2024-07-16 01:19:09 -0500

    services diagram

---
+++ src/components/Module.jsx
@@ -0,0 +1,18 @@
+import { TfiClose } from "react-icons/tfi";
+
+export default function Module() {
+  return (
+    <div className="w-1/2 h-1/2 font-roboto">
+      <button>
+        <TfiClose />
+      </button>
+      <h2 className="text 8xl font-bold">Lorem ipsum dolor</h2>
+      <p className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+        magni! Blanditiis nemo optio explicabo in ea accusamus dolor
+        voluptatibus nisi rerum, modi dolorem necessitatibus corrupti ipsam
+        expedita dicta cum hic.
+      </p>
+    </div>
+  );
+}
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,12 +1,41 @@
 import { Element } from "react-scroll";
+import buttons from "../data/dataButton";
+import Module from "../components/Module";
+import "../index.css";
+import { useState } from "react";
 
 function Services() {
+  const [showModule, setShowModule] = useState(false);
+
   return (
     <Element
       name="servicios"
       id="serivicios"
-      className="w-full h-screen flex justify-center items-start services-bg bg-contain bg-right"
-    ></Element>
+      className="w-full h-screen flex flex-col justify-center items-start services-bg bg-contain bg-right ml-16 pb-18 font-roboto"
+    >
+      <div className="w-1/2">
+        <h2 className="text-7xl font-bold leading-tight pb-8">
+          Simplificamos <br />
+          el mundo jurídico para <br /> estar más cerca de ti
+        </h2>
+        <p className="text-4xl font-medium tracking-wider">
+          ¿Qué servicio buscas?
+        </p>
+      </div>
+      <div className="w-3/6 flex flex-row flex-wrap my-10">
+        {buttons.map((button) => (
+          <button
+            className="btn-services m-4 text-center"
+            key={button.desc}
+            onClick={() => setShowModule(!showModule)}
+          >
+            {button.desc}
+          </button>
+        ))}
+        {showModule && <Module />}
+      </div>
+      <button className="text-xl font-medium btn-more-serv">Ver todos</button>
+    </Element>
   );
 }
 
